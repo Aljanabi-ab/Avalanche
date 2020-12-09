db1850944d34c000681bf5c576b1f6863c5e20bd
Aljanabi-ab <aaljanabi0821@gmail.com>
2020-12-09 10:14:01 +0300

    new

---
+++ aia.php
@@ -0,0 +1,145 @@
+<?php include 'includes/header.php';?>
+
+<head>
+<style type="text/css">
+   table {
+       
+       border: 3px solid black;
+       width: 80%;
+      margin:20px;
+    
+}  
+
+
+
+th, td
+{
+    border: 1px solid black;
+    justify-content:space-evenly;
+    width:25%; 
+}
+ </style>
+
+ </head>
+<body>
+<center>
+<h1> AIA BY ABDULLA </h1>
+<div class="table">
+<table>
+  <caption><b>Client Information</b></caption>
+  <tr>
+    <th>Client's name and final decision maker</th>
+    <td>Samuel</td>
+  </tr>
+  <tr>
+    <th>Client's business (website) name</th>
+    <td>Avalanche Solutionse</td>
+  </tr>
+  <tr>
+    <th>Redesign or brand new website</th>
+    <td>Brand New Website</td>
+  </tr>
+  <tr>
+    <th>Type of website</th>
+    <td>Marketing and identity website </td>
+  </tr>
+  <tr>
+    <th>Client's goals</th>
+    <td>Goal for the website is to have a portfolio of different projects completed, and the marketing of their different services in different sectors. The website should also provide adequate costing for different goods and services as well as contacts</td>
+  </tr>
+</div>
+
+<p>
+
+<div class="table">
+<table>
+ <caption><b>Audience Information</b></caption>
+  <tr>
+    <th>Audience(s)</th>
+    <th>Issues, Concerns, Needs</th>
+    <th>Approach</th>
+  </tr>
+  <tr>
+    <td><h3>Institutions</h3>
+        <p>Government and private sectors, 
+           Medium Sized and Small scale organizations, 
+           Varied resolution sizes,
+        
+        </p></td>
+    <td>
+      <ul>
+        <li>looking for fabrication services</li>
+        <li>Seeking diversity and cretivity in fabrication and design</li>
+        <li>Seeking reliability in engineering solutions</li>
+        <li>Looking for a one stop solution center for technical projects</li>
+        <li>looking for automation solutions</li>
+      </ul>
+    </td>
+    <td>
+      <ul>
+        <li>Provide adequate pictures for previous work</li>
+        <li>Indicate Current Clients</li>
+        <li>Indicate a brief scope of the scale and timeline of the projects</li>
+        <li>Provide an overview of all solutions provided</li>
+      </ul>
+    </td>
+  </tr>
+  <tr>
+    <td><h3>Home and Property Owners</h3>
+        <p>Private developers, Home Owners, and Real Estate managemnt firms, 
+           Computer savvy,
+           PC and Mac user, 
+           Varied resolution sizes
+           
+        </p></td>
+    <td>
+      <ul>
+        <li>Looking for technical maintenance and imrpovement of properties</li>
+        <li>Provison of surveilance systems, provison of power management systems</li>
+        <li>Fast and reliable services</li>
+        <li>Provison of diversifed cost options</li>
+        <li>looking for automation solutions</li>
+      </ul>
+    </td>
+    <td>
+      <ul>
+        <li>Provide adequate pictures for previous work</li>
+        <li>Indicate Current Clients</li>
+        <li>Indicate a brief scope of the scale and timeline of the projects</li>
+        <li>Provide an overview of all solutions provided</li>
+      </ul>
+    </td>
+  </tr>
+  <tr>
+    <td><h3>Makers, Artisans, DIYers</h3>
+        <p>Above 25 years of age,
+           Male & female,
+           Tech savvy,
+           PC and Mac users, 
+           1024 and up resolution,
+           Own computer accessibility/availability, 
+           Always online with varied connections
+        </p></td>
+    <td>
+      <ul>
+        <li>looking for fabrication facilities</li>
+        <li>Looking for prototyping and engineering design facilities</li>
+        <li>Looking for product design and idea to market services</li>
+        <li>looking for automation solutions</li>
+      </ul>
+      </td>
+    <td>
+      <ul>
+        <li>Provide adequate pictures for previous work</li>
+        <li>Provide a list of available fabricating machines and resources</li>
+        <li>Indicate Current Clients</li>
+        <li>Indicate a brief scope of the scale and timeline of the projects</li>
+        <li>Provide an overview of all solutions provided</li>
+      </ul>
+    </td>
+  </tr>
+</table>
+</div>
+</center>
+
+<?php include 'includes/footer.php';?>
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php include 'includes/header.php';?>
+        
+<!-- START LEFT COL -->
+<section>
+ <h2 class="pageID">About ME</h2>
+
+ <img src="images/embedded.jpg" class="embedded" alt="" /><br>
+ <p>My name is ABDULLAH A ALJANABI I am experienced in software development with particular interests in embedded systems.
+ Design is my passion from development, to graphics and evenproducts. I aspire to concentrate in Internet of Things as it is my gateway to
+ improving effciency and optimization in decisions making in the modern world. To achieve this web development is critical as a foundatoon on IoT<p>
+ <P>
+ I also love playing chess and consider it as one of my greatest hobbies<p>
+ </p>
+
+</section>
+
+<!-- END LEFT COL -->
+
+<!-- START RIGHT COL -->
+<aside>
+ <h3>Welcome</h3>
+ <img src="images/iot.jpg" class="iot" alt="" />
+ <br>
+ <img src="images/chess.jpg" class="chess" alt="" />
+ 
+</aside>
+<!-- END RIGHT COL -->
+ 
+<?php include 'includes/footer.php';?>
